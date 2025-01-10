20203e3dafd9d90b0ce903d07325de6b3dc6f552
Shauryan  K <shauryan.kanaujia@gmail.com>
2025-01-09 23:13:07 -0800

    (fix) node

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,17 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Preprocessor for Svelte
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// Use Vercel adapter with a specified Node.js runtime
-		adapter: adapter({
-			runtime: 'nodejs18.x' // Specify Node.js runtime (e.g., 18.x or 20.x)
-		}),
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
